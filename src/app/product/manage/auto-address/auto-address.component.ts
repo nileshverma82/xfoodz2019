29484bb2cc77b4b710bd55b7933a92647c10a0bd
ambikesh1982 <ambikesh1982@gmail.com>
2019-04-21 09:51:27 -0400

    auto address code cleanup

--- src/app/product/manage/auto-address/auto-address.component.ts
+++ src/app/product/manage/auto-address/auto-address.component.ts
@@ -1,6 +1,5 @@
-import { AfterViewInit, Component, ElementRef, Input, NgZone, OnChanges, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, OnInit, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import * as firebase from 'firebase';
 import { Subscription } from 'rxjs';
 import { LocationService } from '../../../core/location.service';
 import { IGeoInfo } from '../../../core/models';
@@ -10,23 +9,16 @@ import { IGeoInfo } from '../../../core/models';
   templateUrl: './auto-address.component.html',
   styleUrls: ['./auto-address.component.scss']
 })
-export class AutoAddressComponent implements OnInit, AfterViewInit, OnChanges, OnDestroy {
+export class AutoAddressComponent implements OnInit, OnChanges {
 
   subscription: Subscription;
 
-  @ViewChild('addessSearch') searchElm: ElementRef;
+  // @ViewChild('addessSearch') searchElm: ElementRef;
   @ViewChild('gmap') mapElm: ElementRef;
   @Input() productForm: FormGroup;
   @Input() userGeoInfo: IGeoInfo;
 
-  geoInfo: IGeoInfo;
-
-  // place: google.maps.places.PlaceResult;
-
-  constructor(private locationService: LocationService, private ngZone: NgZone) {
-    console.log('constructor: #### View Initialized ####');
-  // this.locationService.loadGoogleMapScript(); // Initialize googe-maps
-  }
+  constructor(private locationService: LocationService) {}
 
   ngOnChanges() {
     console.log('#### from ngOnChanges() ####');
@@ -35,9 +27,7 @@ export class AutoAddressComponent implements OnInit, AfterViewInit, OnChanges, O
     }
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 
   patchUserAddress(geoInfo: IGeoInfo) {
     this.productForm.get('addressForm').patchValue(
@@ -49,60 +39,11 @@ export class AutoAddressComponent implements OnInit, AfterViewInit, OnChanges, O
     this.productForm.get('addressForm').disable();
   }
 
-  ngAfterViewInit() {
-    console.log('afterViewInit: #### View Initialized ####');
-
-    this.subscription = this.locationService.isGoogle$.subscribe(
-      google => {
-        if (google) {
-          console.log('##### Google-maps api loaded #####');
-
-          if (this.productForm.get('addressForm.coordinates').value) {
-
-            this.locationService.createMap(
-              this.mapElm,
-              this.productForm.get('addressForm.coordinates').value.latitude,
-              this.productForm.get('addressForm.coordinates').value.longitude
-            );
-
-          }
-
-          const autoComplete = new google.places.Autocomplete(this.searchElm.nativeElement /*, {types: ['geocode']}*/);
-
-          autoComplete.addListener('place_changed', () => {
-            this.ngZone.run(() => {
-
-              const place = autoComplete.getPlace();
-
-              if (place.geometry) {
-                const lat = place.geometry.location.lat();
-                const lng = place.geometry.location.lng();
-
-                const geoPoint = new firebase.firestore.GeoPoint(lat, lng);
-
-                this.productForm.get('addressForm.coordinates').patchValue(geoPoint);
-                this.productForm.get('addressForm.autoAddressFromMap').patchValue(place.formatted_address);
-
-
-                const map = this.locationService.createMap(this.mapElm, lat, lng);
-
-              } else {
-                console.log('Unable to find a place! try again!!');
-                return;
-              }
-            }); // ngZone.run
-          }); // autoComplete.addListener
-        } else {
-          console.log('##### Waiting for Google-maps api ##### ', google);
-        }
-      });
-  }
-
-
-  ngOnDestroy() {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
+  getAddressFromMap(geo: IGeoInfo) {
+    console.log('Address from placeAutoComplete: ', geo);
+    this.productForm.get('addressForm.coordinates').patchValue(geo);
+    this.productForm.get('addressForm.autoAddressFromMap').patchValue(geo.autoAddressFromMap);
+    this.locationService.createMap(this.mapElm, geo.coordinates.latitude, geo.coordinates.longitude);
   }
 
 }
